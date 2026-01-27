b39c0233f51f6b1078d6e4f143a3c8727830fb5e
reza <rezamoods@gmail.com>
2026-01-27 17:39:43 +0330

    feat: add main entry points for gateway and core

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
+	"log"
+	"os"
 	"my-project/internal/adapter/postgres"
 	"my-project/internal/adapter/redis"
-	"my-project/internal/core"
-	"my-project/pkg/logger"
-	"my-project/srv"
-	"os"
 )
 
 func main() {
-	log := logger.Setup()
-	
-	// Init Infra
-	db, _ := postgres.New(os.Getenv("DB_DSN"))
-	rdb := redis.New(os.Getenv("REDIS_ADDR"))
+	// Init infrastructure
+	_, err := postgres.New(os.Getenv("DB_DSN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = redis.New(os.Getenv("REDIS_ADDR"))
 
-	// Init Logic (The Brain)
-	appLogic := core.NewLogic(db, rdb, log)
-
-	// Init Worker Server
-	worker := &srv.WorkerServer{Logic: appLogic}
-	
-	log.Info("Starting Core Worker...")
-	worker.Start()
+	log.Println("Core Worker Started (Listening to Queue)...")
+	select {} // Block forever
 }
